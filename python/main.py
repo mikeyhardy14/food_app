63a5950ee860544de52289466953d87bdeaa054d
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2022-10-26 18:26:11 -0600

    Refactor current files

--- python/main.py
+++ python/main.py
@@ -1,7 +1,11 @@
 import configure_new_token
+import webApplication
 
 if __name__ == '__main__':
     print('Configuring New Token...')
     token = configure_new_token.get_access_token()
     print('Token Configured!')
-    print('')
+    print('Launch Web App')
+    webApplication.app.run()
+
+
