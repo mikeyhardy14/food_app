31d1d29171c302ecf7cd48787f0bde7badfbce46
mikeyhardy14 <98924283+mikeyhardy14@users.noreply.github.com>
2022-11-16 22:14:20 -0700

    Added: HTML Templates

--- python/webApplication.py
+++ python/webApplication.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 app =Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
+@app.route("/about/")
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(deubg=True)
